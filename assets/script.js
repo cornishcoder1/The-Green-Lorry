d1e9492e0594251a3014c33f67f5f1cd87374127
cornishcoder1 <leahfisher07@hotmail.co.uk>
2021-12-22 13:21:13 +0000

    Add event listener to Get Quote button

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,10 @@
 console.log('hello')
+
+//Listen for clicks on Generate Quote button 
+document.getElementById("generateQuote").addEventListener("click", generateQuote);
+
+//Get form values, calculate quote price and produce form output
+function generateQuote() {
+    let typeItem = document.getElementById("typeItem")
+}
+
