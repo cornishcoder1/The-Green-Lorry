9a2faa9bced8606719bf232e1c9824c7d970a235
cornishcoder1 <leahfisher07@hotmail.co.uk>
2021-12-22 13:46:37 +0000

    Create variables to calculate item price and delivery price

--- assets/script.js
+++ assets/script.js
@@ -8,5 +8,14 @@ function generateQuote() {
     let typeItem = document.getElementById("typeItem").value;
     let collectingFrom = document.getElementById("collectingFrom").value;
     let bristolDelivery = document.getElementById("bristolDelivery").value;
+
+    //Get item price
+    let itemPrice = calculatePrice(typeItem, collectingFrom);
+
+    //Get delivery price 
+    let deliveryPrice = calculateDelivery(itemPrice,bristolDelivery);
+
+    //Create output 
+    var theOutput = "<p>Thank you for your quote request</p>";
 }
 
