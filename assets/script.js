5341197ae929d3e7f7f20ee97c77db5f2e65f934
cornishcoder1 <leahfisher07@hotmail.co.uk>
2021-12-22 13:24:28 +0000

    Create variables for form values

--- assets/script.js
+++ assets/script.js
@@ -5,6 +5,8 @@ document.getElementById("generateQuote").addEventListener("click", generateQuote
 
 //Get form values, calculate quote price and produce form output
 function generateQuote() {
-    let typeItem = document.getElementById("typeItem")
+    let typeItem = document.getElementById("typeItem").value;
+    let collectingFrom = document.getElementById("collectingFrom").value;
+    let bristolDelivery = document.getElementById("bristolDelivery").value;
 }
 
